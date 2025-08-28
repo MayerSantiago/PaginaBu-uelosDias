d60646112dc944c07394d5833fd80deba129b3f7
Mayer Beltran <c.tin@iwglogistics.com>
2025-08-28 00:30:07 -0500

    Actualización general:stadisticsS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics /> {/* 👈 esto envía los datos a Vercel */}
       </body>
     </html>
   );
